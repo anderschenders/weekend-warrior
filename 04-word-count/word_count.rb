7ea7ffaba4a5e5d667e13d801eb2774ca00b9c51
Andrea Chen <andreachen.ac@gmail.com>
2017-09-17 11:25:02 -0700

    Add content to posts in code_jounral, add scrolling.

--- 04-word-count/word_count.rb
+++ 04-word-count/word_count.rb
@@ -1,1 +1,14 @@
+def words(phrase)
+  array_of_words = phrase.split
+  # word_count = array_of_words.size
 
+  words_hash = {}
+  array_of_words.each do |word|
+    if words_hash.has_key?(word)
+      words_hash[word] += 1
+    else
+      words_hash[word] = 1
+    end
+  end
+  return words_hash
+end
